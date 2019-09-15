d4887c2e93f026ca1a6ef4c9201bc267644096ae
Martin Monperrus <martin.monperrus@gnieh.org>
2019-09-15 16:47:08 +0200

    @monperrus at work 😺

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,4 +4,5 @@
  * Spoon is available either under the terms of the MIT License (see LICENSE-MIT.txt) of the Cecill-C License (see LICENSE-CECILL-C.txt). You as the user are entitled to choose the terms under which to adopt Spoon.
  */
 module spoon {
+//requires org.apache.logging.log4j;
 }
